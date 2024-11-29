79bf7b5da2eed89700e518a16dd4ec8a8f171a9c
Noah Sheldon <noahsheldon06@gmail.com>
2024-11-29 15:40:28 +0000

    utils function type added

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const convertWeiToEth = (value: any) => {
+export const convertWeiToEth = (
+  value: "string | number | bigint | boolean"
+) => {
   return (BigInt(value) / BigInt(1e18)).toString(); // Convert Wei to ETH as string
 };
